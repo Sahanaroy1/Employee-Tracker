20e7397aeff8a586585eb51cf25bd612593cb496
Sahanaroy1 <sahanaroy20@gmail.com>
2024-04-17 11:15:09 +0100

    Initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department (id, name) VALUES (1, 'Engineering');
+INSERT INTO department (id, name) VALUES (2, 'Legal');
+INSERT INTO department (id, name) VALUES (3, 'Sales');
+INSERT INTO department (id, name) VALUES (4, 'Finance');
+
+INSERT INTO role (id, title, salary, department_id) VALUES (1, 'Software Engineer', 180000, 1);
+INSERT INTO role (id, title, salary, department_id) VALUES (2, 'Lead Engineer', 200000, 1);
+INSERT INTO role (id, title, salary, department_id) VALUES (3, 'Legal Team Lead', 200000, 2);
+INSERT INTO role (id, title, salary, department_id) VALUES (4, 'Lawyer', 180000, 2);
+INSERT INTO role (id, title, salary, department_id) VALUES (5, 'Sales Lead', 100000, 3);
+INSERT INTO role (id, title, salary, department_id) VALUES (6, 'Salesperson' 80000, 3);
+INSERT INTO role (id, title, salary, department_id) VALUES (7, 'Accountant' 150000, 4);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (1, 'John', 'Deacon', 5, NULL);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (2, 'Joseph', 'Nooney', 6, 1);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (3, 'Jaco', 'Pastorius', 3, NULL);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (4, 'Mark', 'King', 1, 3);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (5, 'Marcus', 'Miller', 7, NULL);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (6, 'Clark', 'Stanley', 3, NULL);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (7, 'Victor', 'Street', 4, 6);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (8, 'Smith', 'Flea', 6, 1);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (9, 'Burton', 'Cliff', 1, 3);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (10, 'Lynn', 'Phil', 4, 6);
---
+++ index.js
@@ -0,0 +1,166 @@
+const inquirer = require('inquirer');
+const { MainMenuQuestions, AddDepartmentQuestions, AddRoleQuestions, AddEmployeeQuestions, UdateEmployeeRole, UpdateEmployeeRoleQuestions} = require('./questions.js');
+const EmployeeDatabase = require('./db/EmployeeDatabase.js');
+
+const db = new EmployeeDatabase({
+    host: 'localhost',
+    user: 'root',
+    password: 'Siyona12!',
+    database: 'employee_db'
+});
+
+db.connect();
+
+const doMenuQuestions = () => {
+
+    inquirer
+        .createPromptModule(MainMenuQuestions)
+        .then((response) =>{
+
+            switch (response.option){
+                case 'view_departments':
+                    view_departments();
+                    break;
+                case 'view_roles':
+                    view_roles();
+                    break;
+                case 'view_employees':
+                    view_employees();
+                    break;
+                case 'add_role':
+                    add_role();
+                    break;
+                case 'update_role':
+                    update_role();
+                    break;
+            }
+        })
+}
+
+const view_departments = () => {
+    db.getDepartments().then((results) => {
+        console.table(results);
+        doMenuQuestions();
+    });
+}
+
+const view_roles = () => {
+    db.getRoles().then((results) => {
+        console.table(results);
+        doMenuQuestions();
+    });
+}
+
+const view_employees = () => {
+    db.getEmployee().then((results) => {
+        console.table(results);
+        doMenuQuestions();
+    });
+}
+
+const add_department = () => {
+    inquirer
+        .createPromptModule(AddDepartmentQuestions)
+        .then((response) => {
+            db.addDepartment(response).then((results) => {
+                console.log('\n', results, '\n');
+                doMenuQuestions();
+            });
+        })
+}
+
+const add_role = () => {
+    db.getDepartments().then((results) => {
+
+        const departmentQuestion = AddRoleQuestions[2];
+        results.forEach((department) =>{
+            departmentQuestion.choices.push({
+                value: department.id,
+                name: department.name
+            });
+        });
+
+        inquirer
+        .prompt(AddRoleQuestions)
+        .then((response) => {
+            db.addRole(response).then((results) => {
+                console.log('\n', results, '\n');
+                doMenuQuestions();
+            });
+        })
+    });
+}
+
+const add_employee = () => {
+    db.getRoles().then((results) => {
+
+        const roleQuestions = AddEmployeeQuestions[2];
+        results.forEach((role) => {
+
+            const role_summary = `${role.title} (${role.department_name}: ${role.salary})`;
+            roleQuestions.choices.push({
+                value:role.id,
+                name: role_summary
+            });
+        });
+        db.getEmployee().then((results) => {
+
+            const managerQuestion = AddEmployeeQuestions[3];
+            results.forEach((employee) =>{
+                managerQuestion.choices.push({
+                    value: employee.id,
+                    name: employee.name
+                });
+            });
+
+            managerQuestion.choices.push({
+                value: null,
+                name: 'None'
+            });
+
+            inquirer
+                .prompt(AddDepartmentQuestions)
+                .then((response) => {
+                    db.addEmployee(response).then((results) => {
+                        console.log('\n', results, '\n');
+                        doMenuQuestions();
+                    });
+                })
+        });
+    });
+}
+
+const update_role = () => {
+
+    db.getEmployees().then((results) => {
+
+        const employeeQuestion = UdateEmployeeRoleQuestions[0];
+        results.forEach((employee) => {
+            employeeQuestion.choices.push({
+                value: employee.id,
+                name: employee.name
+            });
+        });
+        db.getRoles().then((results) => {
+            
+            const releQuestion = UpdateEmployeeRoleQuestions[1];
+            results.forEach((role) => {
+                roleQuestion.choices.push({
+                    value:role.id,
+                    name: role.title
+                });
+            });
+
+            inquirer
+                .prompt(UpdateEmployeeRoleQuestions)
+                .then((response) => {
+                    db.updateEmployeeRole(response).then((results) => {
+                        console.log('\n', results, '\n');
+                        doMenuQuestions();
+                    });
+                })
+        });
+    });
+}
+
+doMenuQuestions();
